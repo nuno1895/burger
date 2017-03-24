f5200d7c1d8485d04d954397daca4232c0a1c57f
Nuno Tavares <Nuno@iMac.local>
2017-03-23 21:05:40 -0400

    final with materilzie

--- server.js
+++ server.js
@@ -5,14 +5,14 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 
-var port = 3000;
+var port = process.env.PORT || 8080;
 
 var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static(process.cwd() + "/public"));
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true}));
 
 // Override with POST having ?_method=DELETE
 app.use(methodOverride("_method"));
@@ -26,7 +26,7 @@ app.set("view engine", "handlebars");
 // Import routes and give the server access to them.
 var burgers_controller = require("./controllers/burgers_controller.js");
 
-
+app.use('/' , burgers_controller);
 app.use("/burgers", burgers_controller);
 
 
